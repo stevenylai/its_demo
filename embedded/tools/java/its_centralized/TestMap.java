8190dba71ef7eefde0844d9023d08936b18f44c1
Steven Lai <stevenylai@gmail.com>
2013-06-24 14:27:17 +0800

    Added the send msg function for test map

--- embedded/tools/java/its_centralized/TestMap.java
+++ embedded/tools/java/its_centralized/TestMap.java
@@ -10,6 +10,9 @@ public class TestMap implements ITSSender {
   }
   public void run () {
   }
+  public void sendMsg(int id, int dir, int speed) {
+    System.out.println("Sending to id: "+Integer.toString(id)+", dir: "+Integer.toString(dir)+", speed: "+Integer.toString(speed));
+  }
   public static void main(String[] args) throws Exception {
     TestMap tester = new TestMap();
     Map its_map = new Map(tester);
